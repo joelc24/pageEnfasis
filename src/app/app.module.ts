b5524de8193342ba52beddd67a33a44cb75a4e3e
JoelC24 <libra-marriaga@hotmail.com>
2022-03-05 12:22:14 -0500

    creacion del componente cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { CardsComponent } from './shared/components/cards/cards.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
